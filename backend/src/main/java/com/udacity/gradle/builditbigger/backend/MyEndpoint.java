08ee87fe6ab0e38cb62feb06fad449fda63f9789
Pavlos <msf7415012@teilar.gr>
2018-08-04 15:32:40 +0300

    feat: use javaJokes library on backend library to provide jokes from JavaJokes

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.example.android.javajokes.Joker;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -22,8 +23,10 @@ public class MyEndpoint {
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
+        Joker joker = new Joker();
+        String joke = joker.getJoke();
+        //response.setData("Jolo, " + name);
+        response.setData(joke);
         return response;
     }
 
