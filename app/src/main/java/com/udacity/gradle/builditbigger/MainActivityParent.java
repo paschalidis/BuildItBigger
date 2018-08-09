0c4ea147465d26bd5091c8301b274a315b85148e
Pavlos <msf7415012@teilar.gr>
2018-08-10 01:07:16 +0300

    feat: add progress bar on loading joke

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityParent.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityParent.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.android.jokeactivity.JokeActivity;
 
@@ -17,13 +18,16 @@ import com.example.android.jokeactivity.JokeActivity;
 public class MainActivityParent extends AppCompatActivity implements OnJokeTaskCompleted {
 
     public final static String IDLING_RESOURCE_LOADER = "JOKE_LOADER";
-
     CountingIdlingResource mIdlingResource = new CountingIdlingResource(IDLING_RESOURCE_LOADER);
+
+    private ProgressBar mTellJokeProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTellJokeProgressBar = findViewById(R.id.tell_joke_progress_bar);
     }
 
 
@@ -51,12 +55,14 @@ public class MainActivityParent extends AppCompatActivity implements OnJokeTaskC
 
     public void tellJoke(View view) {
         mIdlingResource.increment();
+        mTellJokeProgressBar.setVisibility(View.VISIBLE);
         new JokeAsyncTask(this).execute();
     }
 
     @Override
     public void onJokeTaskCompleted(String joke) {
         mIdlingResource.decrement();
+        mTellJokeProgressBar.setVisibility(View.INVISIBLE);
         startJokeActivity(joke);
     }
 
