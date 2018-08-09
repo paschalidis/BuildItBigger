5372c8c14f19900d69ab83d23ec60cdc0ac3c3d6
Pavlos Paschalidis <msf7415012@teilar.gr>
2018-08-09 21:08:12 +0300

    feat: add  Interstitial Ads to free version

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -1,16 +1,54 @@
 package com.udacity.gradle.builditbigger.free;
 
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.udacity.gradle.builditbigger.MainActivityParent;
+import com.udacity.gradle.builditbigger.R;
 
 public class MainActivity extends MainActivityParent {
 
+    private InterstitialAd mInterstitialAd;
+    private Button mTellJokeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mTellJokeButton = findViewById(R.id.tell_joke_button);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+
+        mInterstitialAd.loadAd(getAdRequest());
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                mInterstitialAd.loadAd(getAdRequest());
+                MainActivity.super.tellJoke(mTellJokeButton);
+            }
+        });
+    }
+
     @Override
     public void tellJoke(View view) {
-        Toast.makeText(this, "Test Extene", Toast.LENGTH_SHORT).show();
-        super.tellJoke(view);
+        if(mInterstitialAd.isLoaded()){
+            mInterstitialAd.show();
+        } else {
+            Log.d(MainActivity.class.getSimpleName(), "The interstitial wasn't loaded yet.");
+        }
+    }
+
+    private AdRequest getAdRequest(){
+        return new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
     }
 
 }
